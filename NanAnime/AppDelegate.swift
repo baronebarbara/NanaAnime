d151538306bd153337c64329cd552a78355c6222
Barbara Barone <barbara.barone@picpay.com>
2021-12-09 11:45:38 -0300

    importando core no projeto principal

--- NanAnime/AppDelegate.swift
+++ NanAnime/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Core
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        var network = Network()
         // Override point for customization after application launch.
         return true
     }
