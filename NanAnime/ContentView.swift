df6259ae58dd54e6aab5f40d90ad9260901959e4
Barbara Barone <barbara.barone@picpay.com>
2021-12-09 10:37:04 -0300

    Initial Commit

---
+++ NanAnime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NanAnime
+//
+//  Created by Barbara Barone on 09/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
